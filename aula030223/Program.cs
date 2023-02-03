1bf77985a1c49f2dd47dcd7e5066479d5d2e7ee6
felixfabri <felix.fabrijr@gmail.com>
2023-02-03 10:11:24 +0000

    Add project files.

---
+++ aula030223/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula030223
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Robo robo = new Robo();
+
+            CodigoCentral.ComunidadeLogica.Robo robo1 = new CodigoCentral.ComunidadeLogica.Robo();
+
+        }
+    }
+}
+
+class Robo
+{
+
+}
+
+namespace CodigoCentral.ComunidadeLogica
+{
+    class Robo
+    {
+
+    }
+}
